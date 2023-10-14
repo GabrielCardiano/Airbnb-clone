835b4fa0ce089e00d33b2c685508738146a4b882
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-10-14 00:45:09 -0300

    feat: navbar

---
+++ app/components/Container.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { ReactElement } from "react";
+
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+function Container({ children }: ContainerProps): ReactElement {
+  return (
+    <div className="
+    max-w-[2520px]
+    mx-auto
+    xl:px-20
+    md:px-10
+    sm:px-2
+    px-4
+    "
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Container
---
+++ app/components/navbar/MenuItem.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { ReactElement } from "react"
+
+interface MenuItemProps {
+  onClick: () => void,
+  label: string,
+}
+
+function MenuItem({ onClick, label }: MenuItemProps): ReactElement {
+  return (
+    <div
+      onClick={onClick}
+      className="
+      px-4
+      py-3
+      hover:bg-neutral-100
+      transition
+      font-semibold"
+    >
+      {label}
+    </div>
+  )
+}
+
+export default MenuItem
