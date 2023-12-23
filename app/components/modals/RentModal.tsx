6ec1ef22b3b8e13df6e34eb4fc3289ab6207b938
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-12-23 20:58:12 -0300

    feat: create location selection options in RentModal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -7,10 +7,13 @@ import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
+import Map from "../Map";
+import dynamic from "next/dynamic";
 
 enum STEPS {
   CATEGORY = 0,
-  LLOCATION = 1,
+  LOCATION = 1,
   INFO = 2,
   IMAGES = 3,
   DESCRIPTION = 4,
@@ -44,12 +47,17 @@ function RentModal() {
   });
 
   const category = watch('category');
+  const location = watch('location');
+
+  const Map = useMemo(() => dynamic(() => import('../Map'), {
+    ssr: false
+  }), [location]) // Map will be re-rendered everytime location changes;
 
   const setCustomValue = (id: string, value: any) => {
-    setValue(id, value, { 
+    setValue(id, value, {
       shouldValidate: true,
-      shouldDirty: true, 
-      shouldTouch: true, 
+      shouldDirty: true,
+      shouldTouch: true,
     })
   }
 
@@ -95,7 +103,7 @@ function RentModal() {
           <div key={item.label} className="col-span-1">
             <CategoryInput
               onClick={(category) => setCustomValue('category', category)}
-              selected={category === item.label }
+              selected={category === item.label}
               label={item.label}
               icon={item.icon}
             />
@@ -105,11 +113,27 @@ function RentModal() {
     </div>
   )
 
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Where is you place located?"
+          subtitle="Help your guests find you!"
+        />
+        <CountrySelect
+          onChange={(value) => setCustomValue('location', value)}
+          value={location}
+        />
+        <Map center={location?.latlng} />
+      </div>
+    )
+  }
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
